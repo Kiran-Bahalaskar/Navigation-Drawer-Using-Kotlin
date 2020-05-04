4f42279325115d5941795e31f52e46331f41f20c
Kiran <kbahalaskar@rediffmail.com>
2020-05-04 16:44:11 +0530

    From Kiran

---
+++ app/src/main/java/com/example/navigationdrawerusingkotlin/Fragment/Home_Fragment.kt
@@ -0,0 +1,24 @@
+package com.example.navigationdrawerusingkotlin.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.navigationdrawerusingkotlin.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Home_Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_, container, false)
+    }
+
+}
